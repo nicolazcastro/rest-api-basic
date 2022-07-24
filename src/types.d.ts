86efef9356c3502ef6fcfc03acd04adfa7ba608c
nicolas-vexels <93717076+nicolas-vexels@users.noreply.github.com>
2022-07-24 17:24:02 -0300

    Added my project

---
+++ src/types.d.ts
@@ -0,0 +1,11 @@
+import { Visibility, Weather } from "./enums"
+export interface DiaryEntry {
+  id: number
+  date: string
+  weather: Weather
+  visibility: Visibility
+  comment: string
+}
+
+export type NonSensitiveInfoDiaryEntry = Omit<DiaryEntry, 'comment'>
+export type NewDiaryEntry = Omit<DiaryEntry, 'id'>
